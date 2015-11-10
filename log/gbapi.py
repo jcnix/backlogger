f61003677afac96935e8787122b27d616f6a1df5
Casey Jones <jonescaseyb@gmail.com>
2015-11-10 14:13:18 -0600

    URL encode arguments

--- log/gbapi.py
+++ log/gbapi.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-import urllib2
-import json
+import urllib2, urllib, json
 
 class GBApi:
 	def __init__(self):
@@ -8,7 +7,14 @@ class GBApi:
 		self.gb_url = 'http://www.giantbomb.com/api'
 
 	def search(self, query):
-		request = self.gb_url+'/search/?query='+query+'&api_key='+self.api_key+'&resources=game&field_list=name,image&format=json'
+		query_args = {'query': query,
+									'api_key': self.api_key,
+									'resources': 'game',
+									'field_list': 'name,image',
+									'format': 'json'
+									}
+		args = urllib.urlencode(query_args)
+		request = self.gb_url+'/search/?' + args
 		response = urllib2.urlopen(request)
 		r = response.read()
 		j = json.loads(r)
