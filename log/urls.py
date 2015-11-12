fcc3abb8ba8ce70207869c79c82ce733f1101e3a
Casey Jones <jonescaseyb@gmail.com>
2015-11-12 14:23:23 -0600

    Add link to /add/ on profile page

--- log/urls.py
+++ log/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^accounts/profile/$', views.profile),
-	url(r'^add/$', views.search_game),
+	url(r'^add/$', views.search_game, name='search_game'),
 	url(r'^add/(?P<gbid>[0-9]+)/$', views.add_game),
 ]
